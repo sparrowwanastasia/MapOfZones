4060b4a6d8a6c301f146c173acbcaf6a277d9c49
sparrowwanastasia <na659@mail.ru>
2026-06-05 15:04:34 +0300

    add initial calculated data fixture

--- server/build.sh
+++ server/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-
 python manage.py import_districts
+python manage.py loaddata initial_data.jsonport_districts
