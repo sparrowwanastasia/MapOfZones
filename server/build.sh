c38e225167fd0a4f2c4d01e60a65972cebb03e5d
sparrowwanastasia <na659@mail.ru>
2026-06-05 15:35:05 +0300

    load full local data fixture

--- server/build.sh
+++ server/build.sh
@@ -4,6 +4,4 @@ set -e
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py import_districts
-python manage.py loaddata mapapi/fixtures/initial_data.json
-python manage.py loaddata mapapi/fixtures/objects_data.json
+python manage.py loaddata mapapi/fixtures/full_data.json.gz
