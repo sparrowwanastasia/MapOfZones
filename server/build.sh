6f3151c8a694c649185603b1a91f601d00aa5861
sparrowwanastasia <na659@mail.ru>
2026-06-05 13:52:32 +0300

    deploy with districts only

--- server/build.sh
+++ server/build.sh
@@ -5,11 +5,3 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 python manage.py import_districts
-
-python manage.py import_ecology --api-key "$DATA_MOS_API_KEY"
-python manage.py import_social --api-key "$DATA_MOS_API_KEY"
-python manage.py import_noise --api-key "$DATA_MOS_API_KEY"
-
-python manage.py compute_eco_stats
-python manage.py compute_social_stats
-python manage.py compute_noise_stats
